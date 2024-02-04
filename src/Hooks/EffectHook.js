3a51f13c6450d92969af76f20bfe13e5125e3bdb
ummarFurquan <mohammedummar29@gmail.com>
2024-02-04 18:26:00 +0530

    Changes in the useEffect Hook

--- src/Hooks/EffectHook.js
+++ src/Hooks/EffectHook.js
@@ -1,31 +1,68 @@
-import React, {useState, useEffect} from 'react'
+// import React, {useState, useEffect} from 'react'
 
-function EffectHook() {
-    const [resourceType, setResourceType] = useState('posts')
-    const [items, setItems] = useState([])
+// function EffectHook() {
+//     const [resourceType, setResourceType] = useState('posts')
+//     const [items, setItems] = useState([])
 
-    useEffect(()=>{
-        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then(response => response.json())
-      .then(json => setItems(json))
-    },[resourceType])
+//     useEffect(()=>{
+//         fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+//       .then(response => response.json())
+//       .then(json => setItems(json))
+//       // console.log
+//     },[resourceType])
 
+//   return (
+//     <>
+    
+//     <div className='container my-5 ' style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+//         <h1>The useEffect Hook!</h1>
+//     <h4>Click on any button to change the resourceType</h4> 
+//     </div >  
+//     <div className='container' style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+//         <button className='btn btn-outline-primary mx-5' onClick={() => setResourceType('posts')}>Posts</button>
+//         <button className='btn btn-outline-primary mx-5' onClick={() => setResourceType('users')}>Users</button>
+//         <button className='btn btn-outline-primary mx-5' onClick={() => setResourceType('comments')}>Comments</button>
+//     </div>
+//     <div className='container my-5'>
+//     <h1 style={{display:'flex', justifyContent:'center', alignItems:'center'}}>{resourceType}</h1> 
+//     <h3 className='container'>Data from the API</h3>
+//     {items.map(item => {
+//         return <p key={item.id}>{JSON.stringify(item)}</p>})}  
+//     </div>
+//     </>
+//   )
+// }
+
+// export default EffectHook
+
+
+import React, { useEffect, useState } from 'react'
+
+const EffectHook = () => {
+
+  const [resourceType, setResourceType] = useState('posts');
+  const [items, setItems] = useState([])
+
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    .then(response => response.json())
+    .then(json => setItems(json))
+  },[resourceType])
   return (
     <>
-    
-    <div className='container my-5 ' style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+        <div className='container my-5 ' style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
         <h1>The useEffect Hook!</h1>
     <h4>Click on any button to change the resourceType</h4> 
-    </div >  
+    </div> 
     <div className='container' style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-        <button className='btn btn-outline-primary mx-5' onClick={() => setResourceType('posts')}>Posts</button>
-        <button className='btn btn-outline-primary mx-5' onClick={() => setResourceType('users')}>Users</button>
-        <button className='btn btn-outline-primary mx-5' onClick={() => setResourceType('comments')}>Comments</button>
-    </div>
-    <div className='container my-5'>
-    <h1 style={{display:'flex', justifyContent:'center', alignItems:'center'}}>{resourceType}</h1> 
-    <h3 className='container'>Data from the API</h3>
-    {items.map(item => {
+    <button className='btn btn-outline-primary mx-5' onClick={()=> setResourceType('posts')}>Posts</button>  
+    <button className='btn btn-outline-primary mx-5' onClick={()=> setResourceType('users')}>Users</button>  
+    <button className='btn btn-outline-primary mx-5' onClick={() => setResourceType('comments')}>Comments</button>  
+    </div> 
+         <div className='container my-5'>
+   <h1 style={{display:'flex', justifyContent:'center', alignItems:'center'}}>{resourceType}</h1> 
+        <h3 className='container'>Data from the API</h3>
+  {items.map(item => {
         return <p key={item.id}>{JSON.stringify(item)}</p>})}  
     </div>
     </>
